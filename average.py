d02488fece4734cd7f4fc0467dd1e5f4be4f7003
VivGOAT4444 <khuranaviv@gmail.com>
2026-05-03 14:34:52 +0100

    Add files via upload

---
+++ average.py
@@ -0,0 +1,11 @@
+goal1 = 37
+goal2 = 65
+goal3 = 92
+goal4 = 19
+goal5 = 81
+
+sum = goal1+goal2+goal3+goal4+goal5
+print("the sum of all the 5 goals is: ", sum)
+
+average = sum/5
+print("the average of the goals scored which is: ", average)
