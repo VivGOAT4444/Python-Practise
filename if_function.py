fac1fe45d95979601d1254ce2addfb9c66fe4a89
VivGOAT4444 <khuranaviv@gmail.com>
2026-05-10 14:28:01 +0100

    Add files via upload

---
+++ if_function.py
@@ -0,0 +1,19 @@
+
+
+
+#num = 3
+#if num > 0:
+#    print(num, "is a positive number.")
+
+    
+#num = -1
+#if num > 0:
+#    print(num, "is a positive number.")
+    
+
+i = 4
+if i < 15:
+   print(i, "is smaller than 15.")
+
+else:
+    print(i, "is greater than or equal to 15.")
